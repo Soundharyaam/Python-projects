42ac748348ac0078d8937cc86641a257f21feac9
Soundharya AM <soundharyabharatiya@gmail.com>
2018-03-05 18:08:29 +0530

    Ex:12

---
+++ ex12.py
@@ -0,0 +1,7 @@
+y=input("Name?")
+age=input("How old are you?")
+height=input("How tall are you?")
+profession=input("What's your position?")
+
+print(f"So, you're {age} old, {height} tall and work as a {profession}")
+print("How old are you?" , input())
